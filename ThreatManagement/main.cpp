556635539212efd8749bdfc31ba8a2991a7c4284
kali <kaliuwu@icloud.com>
2025-01-23 08:49:00 +0000

    Initial Commit

---
+++ ThreatManagement/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ThreatManagement
+//
+//  Created by Kali  on 23/01/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
